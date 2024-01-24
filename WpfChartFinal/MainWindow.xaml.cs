f65c5cad36d594fce7c082e1ba94ac17575788f0
NickZivkovic1993 <NickZivkovic@proton.me>
2024-01-24 13:25:59 +0100

    Male ispravke u asyncu

--- WpfChartFinal/MainWindow.xaml.cs
+++ WpfChartFinal/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using LiveCharts.Wpf;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -19,21 +20,30 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+/// Izvinjavam se za kranje neuredan kod bio sam malo pd stresom , mozete pogledati moje druge projekte na githubu da vidite kako zapravo kucam
+/// jos jednom izvinite pogledao sam opet , i ja se jedva snalazim
+
+
 namespace WpfChartFinal
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
 
-            InitializeAsync();
+            InitializeAndSavePieChart();
 
         }
-        private async void InitializeAsync()
+
+
+        private async void InitializeAndSavePieChart()
+        {
+            await InitializeAsync();
+            await SavePieChartAsPngAsync();
+        }
+        private async Task InitializeAsync()
         {
             List<RadnikModel> radnici = await Loader();
             LoaderClass loaderInstance = new LoaderClass();
@@ -49,15 +59,13 @@ namespace WpfChartFinal
                 });
             }
 
-            // Use Dispatcher.Invoke to update UI elements from a background thread
             Dispatcher.Invoke(() =>
             {
-                // Reference the PieChart defined in XAML using its name
                 pieChart.Series = serija;
             });
         }
 
-        
+
         public async Task<List<RadnikModel>> Loader()
         {
             //ovo treba da ide drugde u apsettings/json najverovatnije al za trenutne potrebe je dovoljno dobro
@@ -84,11 +92,60 @@ namespace WpfChartFinal
                 return radnici;
 
 
-                //File.WriteAllText("output.html", htmlContent);
+                
             }
         }
 
-        
+        private async Task SavePieChartAsPngAsync()
+        {
+            
+            await SaveAsPngAsync(pieChart);
+        }
+
+        private async Task SaveAsPngAsync(UIElement visual)
+        {
+            await InitializeAsync();
+
+            
+            if (visual.IsMeasureValid && visual.IsArrangeValid &&
+                visual.RenderSize.Width > 0 && visual.RenderSize.Height > 0)
+            {
+                
+                var renderTargetBitmap = new RenderTargetBitmap(
+                    (int)visual.RenderSize.Width,
+                    (int)visual.RenderSize.Height,
+                    96, 
+                    96, 
+                    PixelFormats.Pbgra32);
+
+                
+                renderTargetBitmap.Render(visual);
+
+                //naravoucenije .... ne igraj se asyncom kad radis prvi put nesto....
+                await Task.Delay(1000);
+
+                await Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    
+                    var pngEncoder = new PngBitmapEncoder();
+                    pngEncoder.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+
+                    
+                    string filePath = @"D:\PieChart.png";
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        pngEncoder.Save(fileStream);
+                    }
+                }), System.Windows.Threading.DispatcherPriority.ApplicationIdle);
+            }
+            else
+            {
+                MessageBox.Show("Nece da renderuje kako valje , nema png-a", "Error");
+            }
+        }
+
+
 
     }
 
